59a582a496cc38e9d377b157eef4c7e0e87e0eaa
Pavithra2304 <92969170+Pavithra2304@users.noreply.github.com>
2021-10-28 17:08:37 +0530

    Add files via upload

---
+++ Logicalprogram/Logicalprogram/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logicalprogram
+{
+	class Program
+	{
+		public static void armStrong()
+		{
+			Console.WriteLine("Enter the Number:");
+			int num = Convert.ToInt32(Console.ReadLine());
+			int temp = num;
+			int sum = 0;
+			while(num>0)
+			{
+				int rem = num % 10;
+				sum = sum + rem * rem * rem;
+				num = num / 10;
+			}
+			if(sum == temp)
+			{
+				Console.WriteLine("The Given Number is Armstrong Number");
+			}
+			else
+			{
+				Console.WriteLine("The Given Number is not an Armstrong Number");
+			}
+			
+		}
+	
+		static void Main(string[] args)
+		{
+			armStrong();
+
+		}
+	}
+}
